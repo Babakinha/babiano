25cd4621d9c3881b65884087850eafb3f506dfb1
Babakinha <sangueotario@gmail.com>
2024-09-21 19:29:07 -0300

    Yippe, its nice now :3

--- src/key.rs
+++ src/key.rs
@@ -1,11 +1,86 @@
 use sycamore::prelude::*;
 
+#[derive(Prop)]
+pub struct BabianoKeyProps<'a, G: Html> {
+    #[builder(default)]
+    class: String,
+    #[builder(default)]
+    children: Children<'a, G>,
+
+    frequency: f32,
+    volume: &'a ReadSignal<f32>,
+}
+
 // inline_props for the win!
-#[component(inline_props)]
-fn BabianoKey<'a, G: Html>(cx: Scope<'a>, value: &'a ReadSignal<i32>) -> View<G> {
-    view!  {cx,
-        div(class="my-component") {
-            "Value: " (value.get())
+#[component()]
+pub fn BabianoKey<'a, G: Html>(cx: Scope<'a>, props: BabianoKeyProps<'a, G>) -> View<G> {
+    let children = props.children.call(cx);
+    let playing = create_signal(cx, false);
+    let allowed_to_play = create_signal(cx, false);
+
+
+    #[cfg(client)]
+    let context = web_sys::AudioContext::new().expect("Unable to create AudioContext");
+    #[cfg(client)]
+    let master_volume = context.create_gain().expect("Unable to create gain node");
+    #[cfg(client)]
+    master_volume
+        .connect_with_audio_node(&context.destination())
+        .expect("Unable to connect master");
+
+    #[cfg(client)]
+    let oscillator = context
+        .create_oscillator()
+        .expect("Unable to create oscillator");
+    // 'A' Note (i think)
+    #[cfg(client)]
+    oscillator
+        .frequency()
+        .set_value_at_time(props.frequency, 0.0)
+        .expect("Unable to set tone");
+    #[cfg(client)]
+    oscillator
+        .connect_with_audio_node(&master_volume)
+        .expect("Unable to connect oscillator");
+
+    #[cfg(client)]
+    gloo::console::log!("Hello :3");
+
+
+    #[cfg(client)]
+    let oscillator_clone = oscillator.clone();
+    let on_play = move |_| {
+        if !*allowed_to_play.get() {
+            #[cfg(client)]
+            oscillator_clone.start().unwrap();
+            allowed_to_play.set(true);
+        }
+        playing.set(!*playing.get());
+    };
+
+    #[cfg(client)]
+    let context_clone = context.clone();
+    create_effect(cx, move || {
+        props.volume.track();
+        if *playing.get() {
+            #[cfg(client)]
+            master_volume
+                .gain()
+                .set_value_at_time(*props.volume.get(), context_clone.current_time())
+                .unwrap();
+        } else {
+            #[cfg(client)]
+            master_volume
+                .gain()
+                .set_value_at_time(0.0, context_clone.current_time())
+                .unwrap();
+
+        }
+    });
+
+    view! {cx,
+        button(class=props.class, on:click=on_play) {
+            (children)
         }
     }
 }
--- src/piano.rs
+++ src/piano.rs
@@ -1,90 +1,34 @@
+use crate::key::*;
 use perseus::prelude::*;
 use sycamore::prelude::*;
-use crate::key;
 
 fn piano_page<G: Html>(cx: Scope) -> View<G> {
-    let volume_slider = create_signal(cx, String::from("20.0"));
-    let frequency_slider = create_signal(cx, String::from("220"));
-    let playing = create_signal(cx, false);
+    let volume_slider = create_signal(cx, String::from("10"));
+    let volume_normal = create_memo(cx, || {
+        (*volume_slider.get()).parse::<f32>().unwrap() / 100.0
+    });
 
-    #[cfg(client)]
-    let context = web_sys::AudioContext::new().expect("Unable to create AudioContext");
-    #[cfg(client)]
-    let master_volume = context.create_gain().expect("Unable to create gain node");
-    #[cfg(client)]
-    master_volume
-        .connect_with_audio_node(&context.destination())
-        .expect("Unable to connect master");
-
-    #[cfg(client)]
-    let oscillator = context
-        .create_oscillator()
-        .expect("Unable to create oscillator");
-    // 'A' Note (i think)
-    #[cfg(client)]
-    oscillator
-        .frequency()
-        .set_value_at_time(220.0, 0.0)
-        .expect("Unable to set tone");
-    #[cfg(client)]
-    oscillator
-        .connect_with_audio_node(&master_volume)
-        .expect("Unable to connect oscillator");
-
-    #[cfg(client)]
-    gloo::console::log!("Hello :3");
-
-    #[cfg(client)]
-    let oscillator_clone = oscillator.clone();
     let on_play = move |_| {
-        if *playing.get() {
-            #[cfg(client)]
-            if &*volume_slider.get() == "0" {
-                volume_slider.set(String::from("20"));
-            } else {
-                volume_slider.set(String::from("0"));
-            }
-            return;
-        }
         #[cfg(client)]
-        oscillator_clone.start().unwrap();
-        playing.set(true)
+        if &*volume_slider.get() == "0" {
+            volume_slider.set(String::from("10"));
+        } else {
+            volume_slider.set(String::from("0"));
+        }
     };
 
-    #[cfg(client)]
-    let context_clone = context.clone();
-    create_effect(cx, move || {
-        volume_slider.track();
-        #[cfg(client)]
-        gloo::console::log!("Vol", volume_slider.get().to_string());
-        #[cfg(client)]
-        master_volume
-            .gain()
-            .set_value_at_time(
-                volume_slider.get().parse::<f32>().unwrap() / 100.0,
-                context_clone.current_time(),
-            )
-            .unwrap();
-    });
-
-    create_effect(cx, move || {
-        frequency_slider.track();
-        #[cfg(client)]
-        gloo::console::log!("Freq", frequency_slider.get().to_string());
-        #[cfg(client)]
-        oscillator
-            .frequency()
-            .set_value_at_time(
-                frequency_slider.get().parse::<f32>().unwrap(),
-                context.current_time(),
-            )
-            .unwrap();
-    });
-
     view! {cx,
-        button(on:click=on_play) { "Playyy :3" }
+        button(on:click=on_play) { "Mute ><" }
         input(type="range", min="0", max="100", bind:value=volume_slider, class="slider")
-        input(type="range", min="0", max="1000", bind:value=frequency_slider, class="slider")
+        div(style="padding-top: 300px; padding-bottom: 300px; justify-content: center; height: 120px; display:flex;") {
+            BabianoKey(frequency=261.63, volume=volume_normal) {"C"}
+            BabianoKey(frequency=293.66, volume=volume_normal) {"D"}
+            BabianoKey(frequency=329.63, volume=volume_normal) {"E"}
+            BabianoKey(frequency=349.23, volume=volume_normal) {"F"}
+            BabianoKey(frequency=392.00, volume=volume_normal) {"G"}
+            BabianoKey(frequency=440.00, volume=volume_normal) {"A"}
+            BabianoKey(frequency=493.88, volume=volume_normal) {"B"}
+        }
         p { "I love you ❤️" }
     }
 }
